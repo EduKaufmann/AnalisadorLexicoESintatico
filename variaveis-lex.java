ed10a7137cb2f8716d2b346a8462a4107c1f9561
root <leo-montiel@hotmail.com>
2021-05-23 23:50:22 +0000

    push legal :)

--- variaveis-lex.java
+++ variaveis-lex.java
@@ -4,19 +4,19 @@ import java.util.*;
 %standalone
 %line
 %column
-St = (("int "|"char "|"float ")[a-z0-9]*)
+St = (("int "|"char "|"float ")("_"[a-zA-Z_0-9]*|[a-zA-Z]+[_0-9]*)((", "|",")("_"[a-zA-Z_0-9]*|[a-zA-Z]+[_0-9]*))*)";"
 
 %{
  List<String> st = new ArrayList();
 %}
 
 %eof{
-  System.out.print("Variáveis: ");
+  System.out.print("Variáveis: \n");
   for(int i=0;i<st.size();i++){
     if(i==st.size()-1)
      System.out.println(st.get(i));
     else
-     System.out.print(st.get(i)+", ");
+     System.out.print(st.get(i)+"\n ");
   }
 %eof}
 
